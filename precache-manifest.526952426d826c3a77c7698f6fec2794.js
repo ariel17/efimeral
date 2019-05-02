bee466197427cf6753fa937b7b66e6bca841cd5c
Ariel Gerardo Rios <ariel.rios@mercadolibre.com>
2019-05-02 00:29:41 -0300

    Updates

---
+++ precache-manifest.526952426d826c3a77c7698f6fec2794.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a3bacd0d97d96c3d62d8798cf0d20a",
+    "url": "/efimeral/index.html"
+  },
+  {
+    "revision": "1bef41bd703320881f11",
+    "url": "/efimeral/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "dc8e3bdbf54178951d2c",
+    "url": "/efimeral/static/css/main.908fc77f.chunk.css"
+  },
+  {
+    "revision": "1bef41bd703320881f11",
+    "url": "/efimeral/static/js/2.85fed035.chunk.js"
+  },
+  {
+    "revision": "dc8e3bdbf54178951d2c",
+    "url": "/efimeral/static/js/main.3647387f.chunk.js"
+  },
+  {
+    "revision": "088899ba6a6e34a46155",
+    "url": "/efimeral/static/js/runtime~main.9a19c980.js"
+  },
+  {
+    "revision": "31b209af152a9166e67b07568454a798",
+    "url": "/efimeral/static/media/arch.31b209af.png"
+  },
+  {
+    "revision": "23db0be59ea4c6754fda297f815e2906",
+    "url": "/efimeral/static/media/mm3__.23db0be5.svg"
+  },
+  {
+    "revision": "98db4d6a9efa5794052761d6a576b068",
+    "url": "/efimeral/static/media/mm3__.98db4d6a.ttf"
+  },
+  {
+    "revision": "c406c49e2bf2afeddbea36192f12cf10",
+    "url": "/efimeral/static/media/mm3__.c406c49e.woff"
+  },
+  {
+    "revision": "e1ebb8fb95db228f4225e5a66ffb529d",
+    "url": "/efimeral/static/media/mm3__.e1ebb8fb.eot"
+  }
+]);

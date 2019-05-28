b956eded18b3cf9e7edcf4ce98c38312f66ce889
Ariel Gerardo Rios <ariel.rios@mercadolibre.com>
2019-05-28 19:46:50 -0300

    Implements missing concrete methods.

--- api/sessions/docker.go
+++ api/sessions/docker.go
@@ -3,12 +3,19 @@ package sessions
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/ariel17/efimeral/api/apierrors"
 	"github.com/ariel17/efimeral/api/config"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/mercadolibre/go-meli-toolkit/goutils/logger"
+)
+
+const (
+	hubRepositoryName = "docker.io/ariel17/efimeral"
 )
 
 type Container struct {
@@ -42,11 +49,42 @@ func (dc *dockerClient) Pull(distribution Distribution, tag string) *apierrors.A
 }
 
 func (dc *dockerClient) Create(distribution Distribution, tag string) (*Container, *apierrors.APIError) {
-	return nil, nil
+	image := createImageName(distribution, tag)
+	created, err := dc.c.ContainerCreate(dc.Context(), &container.Config{Image: image}, nil, nil, "")
+	if err != nil {
+		return nil, apierrors.NewInternalServerError(err)
+	}
+
+	if err := dc.c.ContainerStart(dc.Context(), created.ID, types.ContainerStartOptions{}); err != nil {
+		return nil, apierrors.NewInternalServerError(err)
+	}
+
+	return dc.Get(created.ID)
 }
 
 func (dc *dockerClient) Get(id string) (*Container, *apierrors.APIError) {
-	return nil, nil
+	cJSON, err := dc.c.ContainerInspect(dc.Context(), id)
+	if err != nil {
+		return nil, apierrors.NewInternalServerError(err)
+	}
+
+	distribution, tag, apiErr := parseImageName(cJSON.Config.Image)
+	if apiErr != nil {
+		return nil, apiErr
+	}
+
+	createdAt, err := time.Parse(time.RFC3339, cJSON.Created)
+	if err != nil {
+		return nil, apierrors.NewInternalServerError(err)
+	}
+
+	c := Container{
+		ID:           id,
+		Distribution: distribution,
+		Tag:          tag,
+		CreatedAt:    createdAt,
+	}
+	return &c, nil
 }
 
 func (dc *dockerClient) Destroy(id string) *apierrors.APIError {
@@ -83,7 +121,35 @@ func (dc *dockerClient) Context() context.Context {
 }
 
 func createImageName(distribution Distribution, tag string) string {
-	return fmt.Sprintf("docker.io/ariel17/efimeral:%s-%s", distribution, tag)
+	return fmt.Sprintf("%s:%s-%s", hubRepositoryName, distribution, tag)
+}
+
+func parseImageName(image string) (Distribution, string, *apierrors.APIError) {
+	aux := strings.Replace(image, hubRepositoryName, "", 1)
+	aux = strings.Replace(aux, ":", "", 1)
+	parts := strings.Split(aux, "-")
+	if len(parts) != 2 {
+		logger.Errorf("error parsing image name; has invalid parts: %s", nil, image)
+		return "", "", apierrors.NewNotFoundError()
+	}
+	distribution := Distribution(parts[0])
+	tags, found := availableDistributions[distribution]
+	if !found {
+		logger.Errorf("error parsing image name; distribution is invalid: %s", nil, image)
+		return "", "", apierrors.NewNotFoundError()
+	}
+	found = false
+	for _, tag := range tags {
+		if tag == parts[1] {
+			found = true
+		}
+	}
+	if !found {
+		logger.Errorf("error parsing image name; tag is not valid: %s", nil, image)
+		return "", "", apierrors.NewNotFoundError()
+	}
+
+	return distribution, parts[1], nil
 }
 
 func newDockerClient() *dockerClient {

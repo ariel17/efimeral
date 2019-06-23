5d9ab7f3ee69c065ae611510f3b2bbddd79090ec
Ariel Gerardo Rios <ariel.rios@mercadolibre.com>
2019-06-23 19:34:48 -0300

    Adds sentry error management for API errors. Closes #11.

--- api/apierrors/apierrors.go
+++ api/apierrors/apierrors.go
@@ -1,6 +1,12 @@
 package apierrors
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ariel17/efimeral/api/config"
+	"github.com/getsentry/sentry-go"
+)
 
 // APIError is the JSON body to show on a failed response.
 type APIError struct {
@@ -16,6 +22,7 @@ func NewBadRequestError(err error) *APIError {
 		Cause:       err.Error(),
 		Status:      http.StatusBadRequest,
 	}
+	sentry.CaptureException(err)
 	return &apiErr
 }
 
@@ -35,5 +42,18 @@ func NewInternalServerError(err error) *APIError {
 		Cause:       err.Error(),
 		Status:      http.StatusInternalServerError,
 	}
+	sentry.CaptureException(err)
 	return &apiErr
+}
+
+func init() {
+	if config.Environment == config.ProductionEnv {
+		err := sentry.Init(sentry.ClientOptions{
+			Dsn: "https://a07934d966db496eb3a27b003d6e9bfe:49b4d993cc74473bbf9d004edbef7e3c@sentry.io/1488418",
+		})
+
+		if err != nil {
+			panic(fmt.Sprintf("Sentry initialization failed: %v\n", err))
+		}
+	}
 }
--- api/config/values.go
+++ api/config/values.go
@@ -12,6 +12,7 @@ const (
 	TestEnv              = "test"
 	DefaultCPUs          = int64(1)
 	DefaultMemoryInBytes = int64(1024 * 1024 * 1024) // 1 GB
+	SentryDSN            = "https://a07934d966db496eb3a27b003d6e9bfe:49b4d993cc74473bbf9d004edbef7e3c@sentry.io/1488418"
 )
 
 var (

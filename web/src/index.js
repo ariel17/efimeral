70c3311df04c4233321014dc365e7f5eea0e2dfd
Ariel Gerardo Rios <ariel.rios@mercadolibre.com>
2019-07-27 11:13:43 -0300

    Adds Sentry error tracker for web project. Closes #12

--- web/src/index.js
+++ web/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-bootstrap/dist/react-bootstrap.js';
 
-
+Sentry.init({dsn: "https://caa7ef28eb594c8799f16a27b8279d2f@sentry.io/1488420"});
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change

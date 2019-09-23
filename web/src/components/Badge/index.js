7e3e4149b180b8960754faa971f5cdd7f30140b7
Ariel Gerardo Rios <ariel.gerardo.rios@gmail.com>
2019-09-22 21:08:10 -0300

    Fixes badge position. Refs #32.

--- web/src/components/Badge/index.js
+++ web/src/components/Badge/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Img = styled.img`
+    float: left;
+`
 
 
 class Badge extends Component {
     render() {
         return (<a href="https://github.com/ariel17/efimeral">
-            <img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_left_white_ffffff.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" />
+            <Img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_left_white_ffffff.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" />
         </a>);
 
     }

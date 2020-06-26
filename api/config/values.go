118f7a35fd1e946c2fadb8601cbed154c29cead8
Ariel Gerardo Ríos <ariel.rios@mercadolibre.com>
2020-06-26 18:01:43 -0300

    Available distributions based on environment.

--- api/config/values.go
+++ api/config/values.go
@@ -7,20 +7,39 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// Distribution refers to Linux distributions.
+type Distribution string
+
 const (
-	ProductionEnv        = "production"
-	TestEnv              = "test"
-	DefaultCPUs          = int64(1)
-	DefaultMemoryInBytes = int64(1024 * 1024 * 1024) // 1 GB
+	// ProductionEnv is the production environment name.
+	ProductionEnv = "production"
+	// TestEnv test environment name.
+	TestEnv = "test"
+	// DefaultCPUs is the default amount of CPUs to use in each container.
+	DefaultCPUs = int64(1)
+	// DefaultMemoryInBytes is the default amount of memory to use in each
+	// container.
+	DefaultMemoryInBytes              = int64(1024 * 1024 * 1024) // 1 GB
+	ubuntu               Distribution = "ubuntu"
+	arch                 Distribution = "archlinux"
 )
 
 var (
-	Environment        string
-	BaseURL            string
-	Now                func() time.Time
-	SentryDSN          string
+	// Environment is the current environment name.
+	Environment string
+	// BaseURL is the URL prefix where containers will be published.
+	BaseURL string
+	// Now is a time function holder.
+	Now func() time.Time
+	// SentryDSN is the value for Sentry to configure it (by environment).
+	SentryDSN string
+	// MaxSessionDuration is the maximum time allowed for each container.
 	MaxSessionDuration time.Duration
-	CleanTimeLapse     time.Duration
+	// CleanTimeLapse is the time between each execution for the cleaner job.
+	CleanTimeLapse time.Duration
+	// AvailableDistributions is the list of OS and tags availables in this
+	// platform.
+	AvailableDistributions map[Distribution][]string
 )
 
 func loadTestValues() {
@@ -31,6 +50,11 @@ func loadTestValues() {
 	SentryDSN = "https://15932fd4007f4e6d9e105442556df2b6@sentry.io/1488847"
 	MaxSessionDuration = time.Minute
 	CleanTimeLapse = 5 * time.Second
+	AvailableDistributions = map[Distribution][]string{
+		ubuntu: []string{"19.04"},
+		arch:   []string{"latest"},
+	}
+
 }
 
 func loadProductionValues() {
@@ -39,6 +63,11 @@ func loadProductionValues() {
 	SentryDSN = "https://a07934d966db496eb3a27b003d6e9bfe@sentry.io/1488418"
 	MaxSessionDuration = 20 * time.Minute
 	CleanTimeLapse = 10 * time.Minute
+	AvailableDistributions = map[Distribution][]string{
+		ubuntu: []string{"20.04"},
+		arch:   []string{"latest"},
+	}
+
 }
 
 func init() {
